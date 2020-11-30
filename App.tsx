a1a6fda729572c19b04b4db582e9b5ab6b6798d6
Mohammad Arif Shariati <shariati@mohammads-mbp.local>
2020-11-30 18:05:16 +0300

    setting dark mode

--- App.tsx
+++ App.tsx
@@ -10,6 +10,8 @@ export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
+  console.log(colorScheme);
+
   if (!isLoadingComplete) {
     return null;
   } else {
